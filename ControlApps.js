bbcad5567a10d1402f10bd702ff2289974f4b6ff
dilzzmanuel-ux <dilzzmanuel@gmail.com>
2026-01-01 02:33:53 +0700

    Add files via upload

---
+++ ControlApps.js
@@ -0,0 +1,6 @@
+module.exports = {
+  tokens: "8357941989:AAF8XZBzr-h-eHAc0IVyeelHgI8gNtuD90o", 
+  owner: "7393949927", 
+  port: "9362", // Ini Wajib Jangan Diubah
+  ipvps: "https://host.astavvip-tech.xyz" // Jangan Diubah Nanti Eror!!
+};
